12057e0bc296fbc08a9f9943d578bd1b08b7ffec
Solomon T <solomonbstoner@yahoo.com.au>
2025-05-10 12:16:38 +0800

    Untested Assignment 9 driver soln

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -20,6 +20,7 @@
 #include <linux/fs.h> // file_operations
 #include "aesd-circular-buffer.h"
 #include "aesdchar.h"
+#include "aesd_ioctl.h"
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
@@ -35,33 +36,33 @@ void aesd_cleanup_module(void);
 int aesd_release(struct inode *, struct file *);
 loff_t aesd_llseek (struct file *filp, loff_t offset, int whence);
 long aesd_unlocked_ioctl (struct file *filp, unsigned int cmd, unsigned long arg);
-static long aesd_adjust_file_offset(struct file *filp, unsigned int write_cmd, unsigned int write_cmd_offset);
+static loff_t aesd_adjust_file_offset(struct file *filp, size_t write_cmd, size_t write_cmd_offset);
 
 struct aesd_dev aesd_device; // TODO: Qn: why is this global?
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
-    PDEBUG("open");
+	PDEBUG("open");
 	filp->private_data = &aesd_device; // save device struct in private_data of filp
-    return 0;
+	return 0;
 }
 
 int aesd_release(struct inode *inode, struct file *filp)
 {
-    PDEBUG("release");
+	PDEBUG("release");
 	filp->private_data = NULL;
-    return 0;
+	return 0;
 }
 
 ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
-                loff_t *f_pos)
+				loff_t *f_pos)
 {
-    ssize_t retval = 0;
+	ssize_t retval = 0;
 	if (count < 1)
 	{
 		return retval;
 	}
-    PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
+	PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
 	down(&aesd_device.lock); // lock aesd_dev
 	// struct aesd_dev *ad = (struct aesd_dev *)filp->private_data;
 	struct aesd_circular_buffer *cbuf = &aesd_device.cbuf;
@@ -89,13 +90,13 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 	size_t n_b_to_cpy = count > n ? n : count; // Copy partial if count < total length of this entry's string
 	memcpy(ret_str, ent->buffptr+j, n_b_to_cpy);
 	i += n_b_to_cpy;
-    retval = i;
+	retval = i;
 	while (i < count)
-   	{
+	   {
 		ent = aesd_circular_buffer_find_entry_offset_for_fpos(cbuf, *(f_pos)+i, &j);
 		if (ent == NULL)
 		{
-            // Nothing else to copy. Copy what we have to userland
+			// Nothing else to copy. Copy what we have to userland
 			break;
 		}
 		n = ent->size;
@@ -104,7 +105,7 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 		i += n_b_to_cpy;
 		retval = i; // Update the number of characters read
 	}
-    PDEBUG("read: returning string: %s" , ret_str);
+	PDEBUG("read: returning string: %s" , ret_str);
 	if (copy_to_user(buf, ret_str, count) != 0) // Return data from circular buffer using copy_to_user
 	{
 		// Error copying
@@ -114,21 +115,21 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 out:
 	kfree(ret_str); // Safe to kfree NULL
 	up(&aesd_device.lock);// unlock aesd_dev
-    *f_pos += retval; // Update the position of the file
-    return retval;
+	*f_pos += retval; // Update the position of the file
+	return retval;
 }
 
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
-                loff_t *f_pos)
+				loff_t *f_pos)
 {
-    ssize_t retval = -ENOMEM;
-    PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
+	ssize_t retval = -ENOMEM;
+	PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
 	// We wil ignore f_pos
 	down(&aesd_device.lock); // lock aesd_dev
 	// struct aesd_dev *ad = (struct aesd_dev *)filp->private_data;
 	// struct aesd_circular_buffer *cbuf = ad->cbuf;
 	struct aesd_buffer_entry *tmp_kbuf = &aesd_device.tmp_kbuf;
-    PDEBUG("write: kzallocating  %ld bytes", count + tmp_kbuf->size + 1);
+	PDEBUG("write: kzallocating  %ld bytes", count + tmp_kbuf->size + 1);
 	char *kbuf = (char *) kzalloc(count + tmp_kbuf->size + 1, GFP_KERNEL); // tmp_kbuf->size != 0 if there is an existing temporary string. +1 for terminating null
 	if (kbuf == NULL)
 	{
@@ -147,11 +148,11 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
 		retval = -EFAULT;
 		goto free_kbuf;
 	}
-    PDEBUG("write: kbuf = %s", kbuf);
+	PDEBUG("write: kbuf = %s", kbuf);
 	retval = count; // We successfully wrote count number of bytes
 	tmp_kbuf->buffptr = kbuf;
 	tmp_kbuf->size += count;
-    PDEBUG("write: tmp_kbuf->size = %ld, tmp_kbuf->buffptr =  %s", tmp_kbuf->size, kbuf);
+	PDEBUG("write: tmp_kbuf->size = %ld, tmp_kbuf->buffptr =  %s", tmp_kbuf->size, kbuf);
 	bool is_term = (tmp_kbuf->buffptr[tmp_kbuf->size-1] == '\n');
 	if (is_term)
 	{
@@ -169,33 +170,109 @@ free_kbuf:
 	kfree(kbuf);
 out:
 	up(&aesd_device.lock); // unlock aesd_dev
-    return retval;
+	return retval;
 }
 
 loff_t aesd_llseek (struct file *filp, loff_t offset, int whence)
 {
-    // TODO
-    return 0;
+	PDEBUG("llseek %lld offset with %d whence", offset, whence);
+	loff_t ret_val = -EINVAL;
+	down(&aesd_device.lock); // lock aesd_dev to calculate size
+	struct aesd_buffer_entry *tmp = NULL;
+	size_t total_size = 0; // Total number of bytes of all the strings of entries present in the circ buffer
+	struct aesd_circular_buffer *cbuf = &aesd_device.cbuf;
+	uint8_t i = 0;
+	 AESD_CIRCULAR_BUFFER_FOREACH(tmp, cbuf, i)
+	{
+		if (tmp == NULL) break;
+		total_size += tmp->size;
+	}
+	up(&aesd_device.lock); // unlock aesd_dev
+	ret_val = generic_file_llseek_size(filp, offset, whence, total_size, total_size); // Kernel function that handles the filp. We don't deal with the device anymore
+	return ret_val;
 }
 
 long aesd_unlocked_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 {
-    // TODO: handle ioctl and call aesd_adjust_file_offset
-    return 0;
+	// No need to lock device in this function since we are not modifying it. Device will only be modified (and hence locked) in the callees
+	long ret_val = -EINVAL;
+	PDEBUG("ioctl called");
+	if (_IOC_TYPE(cmd) != AESD_IOC_MAGIC) /* Wrong driver */
+	{
+		ret_val = -ENOTTY;
+		goto out;
+	}
+	if (_IOC_NR(cmd) > AESDCHAR_IOC_MAXNR) /* Exceeds supported command */
+	{
+		ret_val = -ENOTTY;
+		goto out;
+	}
+	struct aesd_seekto  __user *usr_arg = (struct aesd_seekto __user *) arg;
+	switch (cmd)
+	{
+		case AESDCHAR_IOCSEEKTO:
+			PDEBUG("ioctl runs cmd AESDCHAR_IOCSEEKTO");
+			struct aesd_seekto *k_arg = NULL;
+			if(copy_from_user(k_arg, usr_arg, sizeof(struct aesd_seekto)) != 0)
+			{
+				// handle failed copy
+				PDEBUG("ioctl copy_from_user error");
+				ret_val = -EFAULT;
+			}
+			ret_val = aesd_adjust_file_offset(filp, k_arg->write_cmd, k_arg->write_cmd_offset);
+			break;
+		default: 
+			PDEBUG("ioctl invalid command %u", cmd);
+			ret_val = -EINVAL;
+			goto out;
+	}
+out:
+	return ret_val;
 }
 
 /**
  * Adjust the file offset (f_pos) parameter of @param filp based on the location specified by
  * @param write_cmd (the zero referenced command to locate)
  * and @param write_cmd_offset (the zero referenced offset into the command)
- * @return 0 if successful, negative if error occurred
+ * @return the new offset if successful, negative if error occurred
  *  -ERESTARTSYS if mutex could not be obtained
  *  -EINVAL if write command or write_cmd_offset was out of range
  */
-static long aesd_adjust_file_offset(struct file *filp, unsigned int write_cmd, unsigned int write_cmd_offset)
+static loff_t aesd_adjust_file_offset(struct file *filp, size_t write_cmd, size_t write_cmd_offset)
 {
-    // TODO
-    return 0;
+	loff_t ret_val = 0;
+	PDEBUG("aesd_adjust_file_offset called with write_cmd=%ld and write_cmd_offset=%ld", write_cmd, write_cmd_offset);
+	down(&aesd_device.lock);
+	struct aesd_circular_buffer *cbuf = &aesd_device.cbuf;
+	if (cbuf->entry[write_cmd].size > write_cmd_offset)
+	{
+		// write_cmd_offset exceeds the number of bytes in entry[write_cmd] of the circ buf
+		ret_val = -EINVAL;
+		goto out;
+	}
+	loff_t abs_offset = write_cmd_offset;
+	loff_t total_size = 0;
+	uint8_t i = 0;
+	struct aesd_buffer_entry *entry = NULL;
+	AESD_CIRCULAR_BUFFER_FOREACH(entry, cbuf, i)
+	{
+		if (entry == NULL)
+		{
+			// write_cmd exceeds number of avail entries in the circ buf
+			ret_val = -EINVAL;
+			goto out;
+		}
+		total_size += entry->size;
+		if (i < write_cmd)
+			abs_offset += entry->size;
+	}
+	PDEBUG("aesd_adjust_file_offset calc abs offset = %lld", abs_offset);
+	up(&aesd_device.lock);
+	ret_val = generic_file_llseek_size(filp, abs_offset, SEEK_SET, total_size, total_size); // Kernel function that handles the filp. We don't deal with the device anymore
+	return ret_val;
+out:
+	up(&aesd_device.lock);
+	return ret_val;
 }
 
 struct file_operations aesd_fops = {
@@ -210,52 +287,52 @@ struct file_operations aesd_fops = {
 
 static int aesd_setup_cdev(struct aesd_dev *dev)
 {
-    int err, devno = MKDEV(aesd_major, aesd_minor);
+	int err, devno = MKDEV(aesd_major, aesd_minor);
 
-    cdev_init(&dev->cdev, &aesd_fops);
-    dev->cdev.owner = THIS_MODULE;
-    dev->cdev.ops = &aesd_fops;
-    err = cdev_add (&dev->cdev, devno, 1); // After this call, the aesd dev should be ready to handle all ops from the kernel
-    if (err) {
-        printk(KERN_ERR "Error %d adding aesd cdev", err);
-    }
-    return err;
+	cdev_init(&dev->cdev, &aesd_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &aesd_fops;
+	err = cdev_add (&dev->cdev, devno, 1); // After this call, the aesd dev should be ready to handle all ops from the kernel
+	if (err) {
+		printk(KERN_ERR "Error %d adding aesd cdev", err);
+	}
+	return err;
 }
 
 
 
 int aesd_init_module(void)
 {
-    dev_t dev = 0;
-    int result;
-    result = alloc_chrdev_region(&dev, aesd_minor, 1,
-            "aesdchar");
-    aesd_major = MAJOR(dev);
-    if (result < 0) {
-        printk(KERN_WARNING "Can't get major %d\n", aesd_major);
-        return result;
-    }
-    memset(&aesd_device,0,sizeof(struct aesd_dev)); // cbuf and tmp_kbuf are all cleared too
+	dev_t dev = 0;
+	int result;
+	result = alloc_chrdev_region(&dev, aesd_minor, 1,
+			"aesdchar");
+	aesd_major = MAJOR(dev);
+	if (result < 0) {
+		printk(KERN_WARNING "Can't get major %d\n", aesd_major);
+		return result;
+	}
+	memset(&aesd_device,0,sizeof(struct aesd_dev)); // cbuf and tmp_kbuf are all cleared too
 	sema_init(&aesd_device.lock, 1); // init aesddev semaphore as a mutex
 	down(&aesd_device.lock);
 	aesd_device.cbuf.full = false;
 	aesd_device.tmp_kbuf.buffptr = NULL;
 	aesd_device.tmp_kbuf.size = 0;
 	up(&aesd_device.lock);
-    result = aesd_setup_cdev(&aesd_device);
+	result = aesd_setup_cdev(&aesd_device);
 
-    if( result ) {
-        unregister_chrdev_region(dev, 1);
-    }
-    return result;
+	if( result ) {
+		unregister_chrdev_region(dev, 1);
+	}
+	return result;
 
 }
 
 void aesd_cleanup_module(void)
 {
-    dev_t devno = MKDEV(aesd_major, aesd_minor);
+	dev_t devno = MKDEV(aesd_major, aesd_minor);
 
-    cdev_del(&aesd_device.cdev);
+	cdev_del(&aesd_device.cdev);
 
 	down(&aesd_device.lock); // lock aesddev
 	// iterate through the circular buffer and free all kmalloced  memory
@@ -268,7 +345,7 @@ void aesd_cleanup_module(void)
 	kfree(aesd_device.tmp_kbuf.buffptr); // Safe to free null ptr
 	up(&aesd_device.lock); // unlock aesddev lock
 	// TODO Is there a need to destroy the lock?
-    unregister_chrdev_region(devno, 1);
+	unregister_chrdev_region(devno, 1);
 }
 
 
